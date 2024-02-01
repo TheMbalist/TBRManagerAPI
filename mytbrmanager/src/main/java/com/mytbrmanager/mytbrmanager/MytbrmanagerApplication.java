557f11d9398a1c2a7ee963c9c93b22f1fa2f85b9
Mbali Nkosi <mbaalee25@gmail.com>
2024-02-01 21:38:16 +0200

    Thursday 01 February --Initial commit; spring apllication initialised

---
+++ mytbrmanager/src/main/java/com/mytbrmanager/mytbrmanager/MytbrmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mytbrmanager.mytbrmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytbrmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytbrmanagerApplication.class, args);
+	}
+
+}
